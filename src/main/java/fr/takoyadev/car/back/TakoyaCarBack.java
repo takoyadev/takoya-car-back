512fef1d29cae2a4a83be0aa8c15301e63a51627
chinjto <contact@cyril-defaye.fr>
2021-08-31 00:15:11 +0200

    chinjto_reroll_project
    Add first controller : list cars

--- src/main/java/fr/takoyadev/car/back/TakoyaCarBack.java
+++ src/main/java/fr/takoyadev/car/back/TakoyaCarBack.java
@@ -2,6 +2,7 @@ package fr.takoyadev.car.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class TakoyaCarBack {
---
+++ src/main/java/fr/takoyadev/car/back/controller/CarController.java
@@ -0,0 +1,24 @@
+package fr.takoyadev.car.back.controller;
+
+import fr.takoyadev.car.back.entity.Car;
+import fr.takoyadev.car.back.repository.CarRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/cars")
+public class CarController {
+
+    @NonNull
+    private CarRepository repository;
+
+    @GetMapping
+    public Iterable<Car> list(){
+        return repository.findAll();
+    }
+
+}
