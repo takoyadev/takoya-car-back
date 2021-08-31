f23263878bb152d8d6387a2da9014b987af24596
chinjto <contact@cyril-defaye.fr>
2021-08-31 03:44:09 +0200

    chinjto_reroll_project
    Code smell (set final all variables, as possible)

--- src/main/java/fr/takoyadev/car/back/TakoyaCarBack.java
+++ src/main/java/fr/takoyadev/car/back/TakoyaCarBack.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 public class TakoyaCarBack {
 
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(TakoyaCarBack.class, args);
     }
 
