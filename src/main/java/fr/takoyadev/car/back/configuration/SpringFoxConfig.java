33146d275f80fabb42c1e9312100f5b1b9fdb346
chinjto <contact@cyril-defaye.fr>
2021-08-31 03:42:07 +0200

    chinjto_reroll_project
    Implementing others CRUD API for cars + Add some Swagger descriptions

--- src/main/java/fr/takoyadev/car/back/configuration/SpringFoxConfig.java
+++ src/main/java/fr/takoyadev/car/back/configuration/SpringFoxConfig.java
@@ -2,19 +2,32 @@ package fr.takoyadev.car.back.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SpringFoxConfig {
 
+    public static final String TAG_CARS = "cars";
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder()
+                        .title("Takoya Dev' - Car - Back")
+                        .description("Takoya Dev' CRUD API for car servicing management")
+                        .version("0.0.1-SNAPSHOT")
+                        .build())
+                .tags(
+                        new Tag(TAG_CARS, "Endpoints for CRUD operations on cars")
+                )
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("fr.takoyadev.car.back"))
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build();
     }
--- src/main/java/fr/takoyadev/car/back/controller/CarController.java
+++ src/main/java/fr/takoyadev/car/back/controller/CarController.java
@@ -1,13 +1,19 @@
 package fr.takoyadev.car.back.controller;
 
+import fr.takoyadev.car.back.configuration.SpringFoxConfig;
 import fr.takoyadev.car.back.entity.Car;
 import fr.takoyadev.car.back.repository.CarRepository;
+import io.swagger.annotations.Api;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.NoSuchElementException;
+
+@Api(tags = {SpringFoxConfig.TAG_CARS})
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/cars")
@@ -19,6 +25,39 @@ public class CarController {
     @GetMapping
     public Iterable<Car> list() {
         return repository.findAll();
+    }
+
+    @PostMapping
+    public Car add(@RequestBody final Car item){
+        return repository.save(item);
+    }
+
+    @PutMapping
+    public Car update(@RequestBody final Car item) {
+        return repository.save(item);
+    }
+
+    @DeleteMapping
+    public void delete(@RequestBody final Car item) {
+        repository.delete(item);
+    }
+
+    @GetMapping("/{id}")
+    public Car show(@PathVariable("id") final Long id) {
+        try {
+            return repository.findById(id).get();
+        } catch (final NoSuchElementException exception) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Car not found", exception);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") final Long id) {
+        try {
+            repository.deleteById(id);
+        } catch (final EmptyResultDataAccessException exception) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Car not found", exception);
+        }
     }
 
 }
