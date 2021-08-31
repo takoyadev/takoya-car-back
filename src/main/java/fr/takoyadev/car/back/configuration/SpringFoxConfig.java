8797f888fe62180276d79afd8cd6b1421fa6aeb8
chinjto <contact@cyril-defaye.fr>
2021-08-31 02:32:57 +0200

    chinjto_reroll_project
    Swagger add

---
+++ src/main/java/fr/takoyadev/car/back/configuration/SpringFoxConfig.java
@@ -0,0 +1,22 @@
+package fr.takoyadev.car.back.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SpringFoxConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("fr.takoyadev.car.back"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+}
