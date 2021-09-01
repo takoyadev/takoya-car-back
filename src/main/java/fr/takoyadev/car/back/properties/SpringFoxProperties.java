356ac44a094fea9b4a841e99324bd7c2c6ec02a6
chinjto <contact@cyril-defaye.fr>
2021-09-01 02:13:39 +0200

    chinjto_reroll_project
    Clean imports

--- src/main/java/fr/takoyadev/car/back/properties/SpringFoxProperties.java
+++ src/main/java/fr/takoyadev/car/back/properties/SpringFoxProperties.java
@@ -2,7 +2,6 @@ package fr.takoyadev.car.back.properties;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
